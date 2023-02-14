03273465b5fc33ea5f1c91ae1d51661340e9e4b1
Korryn <kav32@njit.com>
2023-02-14 12:30:23 -0500

    function for database connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,13 +1,15 @@
 #!/usr/bin/php
 <?php
 
-$mydb = new mysqli('localhost', 'kav32', 'spring23', 'IT490');
+function dbConnection() {
+	$mydb = new mysqli('localhost', 'kav32', 'spring23', 'IT490');
 
-if ($mydb->errno != 0) {
+	if ($mydb->errno != 0) {
 	echo "Failed to connect to database: ". #mydb->error . PHP_EOL;
 	exit(0);
-}
+	}
 
 echo "Successfully connected to database.".PHP_EOL;
+return $mydb;
 
 ?>
