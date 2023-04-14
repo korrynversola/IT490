bf9395c5dd87209a9ee9a4d2daea437a2d97f160
Korryn <kav32@njit.com>
2023-04-14 19:58:48 -0400

    updated current version

--- rabbitServer.php
+++ rabbitServer.php
@@ -6,7 +6,7 @@ require_once('rabbitMQLib.inc');
 require_once('login.php.inc');
 
 require_once('dbConnection.php'); //connects to the database
-
+require_once('rabbitFunctions.php'); //functions for server
 function requestProcessor($request) {
         echo "received request".PHP_EOL;
         var_dump($request);
@@ -20,10 +20,11 @@ function requestProcessor($request) {
         if($validSession == 0) {
                 return json_encode(['valid' => 0]);
         }
-
-       // switch ($request['type']) {
-//	case:
-	}*/
+	*/
+       switch ($request['type']) {
+       		case "addBundle":
+			return addBundle($request['bundleName'], $request['bundlePath']);
+	}
 }
 $appServer = new rabbitMQServer("rabbitServer.ini","rabbitServer");
 
